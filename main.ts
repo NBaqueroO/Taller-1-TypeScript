2568b60a5f50f23c3f4afb2779380cac51999487
NBaqueroO <n.baqueroo@uniandes.edu.co>
2024-10-20 14:13:53 -0500

    Detalles adicionales para poder leer la descripcion completa

--- main.ts
+++ main.ts
@@ -14,11 +14,11 @@ export const series = [
         "https://www.hbo.com/game-of-thrones", "./Images/got.jpeg"
     ),
     new Serie(4, "The Big Bang Theory", "CBS", 12,
-        "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else. Enter beautiful, street-smart neighbor Penny, who aims to teach them a thing or two about life.",
+        "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else. Enter beautiful, street-smart neighbor Penny, who aims to teach them a thing or two about life. Despite their on-again, off-again relationship in the past, Leonard and Penny have finally gotten married. Even Sheldon has found a female companion, entering into a relationship agreement with neurobiologist Amy Farrah Fowler, and he recently took their relationship to the next level by marrying her after a long courtship. In their free time, Leonard and Sheldon enjoy fantasy role-playing games with their ever-expanding universe of friends, including fellow scientists Koothrappali, Wolowitz, and Wolowitz’s adorable microbiologist wife, Bernadette, who is adjusting to life with their two children.",
         "https://www.cbs.com/shows/big_bang_theory/about/", "./Images/tbbt.jpeg"
     ),
     new Serie(5, "Sherlock", "BBC", 4,
-        "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London.",
+        "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson (Martin Freeman), who has returned from military service in Afghanistan with the Royal Army Medical Corps",
         "https://www.bbc.co.uk/programmes/b018ttws", "./Images/sherlock.jpeg"
     ),
     new Serie(6, "A Very English Scandal", "BBC", 2,
